12a48db6b4f67e10ab866bb84434370e38dcb83a
Justin Yum <justinyum98@gmail.com>
2023-03-07 16:10:03 -0800

    initial commit

---
+++ MyMusicList/MyMusicListApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyMusicListApp.swift
+//  MyMusicList
+//
+//  Created by Justin Yum on 3/7/23.
+//
+
+import SwiftUI
+
+@main
+struct MyMusicListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
